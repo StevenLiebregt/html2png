4fa17c8a8601654d7b0389e441bafc0407aabb92
Steven Liebregt <s.liebregt@outlook.com>
2017-12-13 16:15:02 +0100

    working, now to finish it

--- js/app.js
+++ js/app.js
@@ -1,21 +1,28 @@
-(function() {
+$(function() {
 
-    var preview = $('#preview')[0].contentWindow.document;
-
-    function getHtml() {
-        return $('#editor-html').val();
+    function insertBootstrapCss() { // css/bootstrap.min.css
+        // TODO
     }
 
-    function getCss() {
-        return $('#editor-css').val();
+    function insertJqueryJs() { // js/jquery.min.js
+        // TODO
     }
 
+    function insertBootstrapJs() { // js/bootstrap.min.js
+        insertJqueryJs();
+        // TODO
+    }
+
+    /**
+     * On editing one of the editors, refresh the preview window
+     */
     $('.editor').on('keyup', function() {
+        var preview = $('#preview')[0].contentWindow.document;
         preview.open();
         preview.close();
 
-        $('body', preview).append(getHtml());
-        $('head', preview).append('<style>' + getCss() + '</style>');
+        $('body', preview).append($('#editor-html').val());
+        $('head', preview).append('<style>' + $('#editor-css').val() + '</style>');
     });
 
 });
